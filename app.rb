fb21be48d9c303557fb82cf54d913b3cac2176cb
NoFoster <kfoster0@chicagobooth.edu>
2025-04-18 05:07:55 +0000

    Homepage creation

--- app.rb
+++ app.rb
@@ -1,20 +1,35 @@
 require "sinatra"
 require "sinatra/reloader"
 require "dotenv/load"
+require "http"
+require "json"
 
-ENV.fetch("EXCHANGE_RATE_KEY")
+er_key = ENV.fetch("EXCHANGE_RATE_KEY")
+
+exchange_rate_url = "https://api.exchangerate.host/list?access_key=" + er_key
+raw_response = HTTP.get(exchange_rate_url)
+parsed_response = JSON.parse(raw_response)
+currency_hash = parsed_response.fetch("currencies")
 
 get("/") do
 
+ @currency_list = currency_hash.keys.sort
   erb(:homepage)
 end
 
 get("/:primary") do
+@primary = params.fetch("primary")
+
 
   erb(:primary_list)
 end
 
+
+
 get("/:primary/:secondary") do
+@primary = params.fetch("primary")
+@secondary = params.fetch("secondary")
+
 
   erb(:secondary)
 end
