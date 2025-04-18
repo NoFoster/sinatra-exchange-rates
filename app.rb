81f4de3f259e627f70cf1a7baf3c361b4c8390c7
NoFoster <kfoster0@chicagobooth.edu>
2025-04-18 18:27:48 +0000

    90percentdone

--- app.rb
+++ app.rb
@@ -17,19 +17,22 @@ get("/") do
   erb(:homepage)
 end
 
+
 get("/:primary") do
-@primary = params.fetch("primary")
-
-
+  @primary = params.fetch("primary")
+  @countries = currency_hash.keys.sort
   erb(:primary_list)
 end
 
 
-
 get("/:primary/:secondary") do
-@primary = params.fetch("primary")
-@secondary = params.fetch("secondary")
+  @primary = params.fetch("primary")
+  @secondary = params.fetch("secondary")
 
+  conversion_rate_url = "https://api.exchangerate.host/convert?from=#{@primary}&to=#{@secondary}&amount=1&access_key=" + er_key
+conv_raw_response = HTTP.get(conversion_rate_url)
+con_parsed_response = JSON.parse(conv_raw_response)
+@conversion = con_parsed_response.fetch("result")
 
   erb(:secondary)
 end
