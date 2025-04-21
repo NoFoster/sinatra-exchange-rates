09ad0b8effee003d50581d6418080a047bffa598
NoFoster <kfoster0@chicagobooth.edu>
2025-04-21 21:17:28 +0000

    reframedforbob

--- app.rb
+++ app.rb
@@ -4,23 +4,27 @@ require "dotenv/load"
 require "http"
 require "json"
 
-er_key = ENV.fetch("EXCHANGE_RATE_KEY")
-
-exchange_rate_url = "https://api.exchangerate.host/list?access_key=" + er_key
-raw_response = HTTP.get(exchange_rate_url)
-parsed_response = JSON.parse(raw_response)
-currency_hash = parsed_response.fetch("currencies")
 
 get("/") do
 
- @currency_list = currency_hash.keys.sort
+@raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch('EXCHANGE_RATE_KEY')}")
+@raw_string = @raw_response.to_s
+@parsed_response = JSON.parse(@raw_string)
+@currency_hash = @parsed_response.fetch("currencies")
+
+ @currency_list = @currency_hash.keys
   erb(:homepage)
 end
 
 
 get("/:primary") do
   @primary = params.fetch("primary")
-  @countries = currency_hash.keys.sort
+
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch('EXCHANGE_RATE_KEY')}")
+@raw_string = @raw_response.to_s
+@parsed_response = JSON.parse(@raw_string)
+@countries= @parsed_response.fetch("currencies")
+
   erb(:primary_list)
 end
 
@@ -29,10 +33,11 @@ get("/:primary/:secondary") do
   @primary = params.fetch("primary")
   @secondary = params.fetch("secondary")
 
-  conversion_rate_url = "https://api.exchangerate.host/convert?from=#{@primary}&to=#{@secondary}&amount=1&access_key=" + er_key
-  conv_raw_response = HTTP.get(conversion_rate_url)
-  con_parsed_response = JSON.parse(conv_raw_response)
-  @conversion = con_parsed_response.fetch("result")
+  
+  @conv_raw_response = HTTP.get("https://api.exchangerate.host/convert?from=#{@primary}&to=#{@secondary}&amount=1&access_key=#{ENV.fetch('EXCHANGE_RATE_KEY')}")
+  @conv_string_response= @conv_raw_response.to_s
+  @con_parsed_response = JSON.parse(@conv_string_response)
+  @conversion = @con_parsed_response.fetch("result")
 
   erb(:secondary)
 end
